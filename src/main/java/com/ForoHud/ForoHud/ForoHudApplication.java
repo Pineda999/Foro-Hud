79efceb6aebd0e67fc2280f522e313f682f3e81c
pineda999 <pinedamoises08@gmail.com>
2025-08-13 16:01:48 -0600

    Subiendo proyecto Foro Hud

---
+++ src/main/java/com/ForoHud/ForoHud/ForoHudApplication.java
@@ -0,0 +1,13 @@
+package com.ForoHud.ForoHud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ForoHud/ForoHud/ForoHudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForoHud.ForoHud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
